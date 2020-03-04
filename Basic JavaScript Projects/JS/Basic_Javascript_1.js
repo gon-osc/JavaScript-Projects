b42ab272222ac12a2392e1e3ed5bc096e80d5dcc
Oscar Gonzalez <gonzalezoscarsalvador@gmail.com>
2020-03-03 23:09:59 -0700

    Basic JavaScript Projects

---
+++ Basic JavaScript Projects/JS/Basic_Javascript_1.js
@@ -0,0 +1,7 @@
+window.alert("Hello, World!");
+
+function My_First_Function() {
+    var str = "This text is green!";
+    var result = str.fontcolor("green");
+    document.getElementById("Green_text").innerHTML = result;
+}
