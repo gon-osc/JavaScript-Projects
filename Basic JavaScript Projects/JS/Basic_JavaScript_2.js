b29f5826ae3a20f38bdae280fe41726afbac35a7
Oscar Gonzalez <gonzalezoscarsalvador@gmail.com>
2020-03-05 13:40:03 -0700

    Basic_JavaScript_2_Assignments

---
+++ Basic JavaScript Projects/JS/Basic_JavaScript_2.js
@@ -0,0 +1,9 @@
+function displayDate() {
+    document.getElementById("demo").innerHTML = Date();
+}
+
+function myFunction() {
+    var sentence = "I am learning";
+    sentence += " a lot from this course!";
+    document.getElementById("Concatenate").innerHTML = sentence;
+}
